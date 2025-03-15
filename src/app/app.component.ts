e4ac81696f3572fa99f1fe083d499948b0060657
serene.mathew <serene.mathew@haatch.in>
2025-03-15 12:07:37 +0530

    feat(routing): set up application rounting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './features/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
+  {
+    path: "cocktails", children: [
+      { path: "", pathMatch: "full", loadComponent: () => import('./features/cocktail-list/cocktail-list.component').then(m => m.CocktailListComponent) },
+      { path: ":id", loadComponent: () => import('./features/cocktail-details/cocktail-details.component').then(m => m.CocktailDetailsComponent) },
+    ]
+  },
+];
